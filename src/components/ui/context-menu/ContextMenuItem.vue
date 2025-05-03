f0e17fdc9a7543f75bf53a3a4ffc1f133d8943d1
Renkin <906155099@qq.com>
2025-05-03 18:54:31 +0800

    更新样式和组件，优化用户体验
    1. 在main.ts中引入shadcn.css和global.css，增强样式管理。
    2. 在shadcn.css中调整光主题和暗主题的ring颜色，提升视觉一致性。
    3. 在ContextMenuItem.vue中优化destructive变体的hover和focus状态样式，改善用户交互体验。

--- src/components/ui/context-menu/ContextMenuItem.vue
+++ src/components/ui/context-menu/ContextMenuItem.vue
@@ -34,18 +34,16 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
     v-bind="forwarded"
     :class="cn(
       `focus:bg-accent focus:text-accent-foreground 
-      data-[variant=destructive]:text-destructive-foreground
-      data-[variant=destructive]:bg-destructive/10
       data-[variant=destructive]:hover:bg-destructive/80
-      data-[variant=destructive]:hover:text-white
+      data-[variant=destructive]:hover:text-gray-800
       dark:data-[variant=destructive]:hover:bg-destructive/80
-      dark:data-[variant=destructive]:hover:text-white
+      dark:data-[variant=destructive]:hover:text-gray-800
       data-[variant=destructive]:focus:bg-destructive/80
-      data-[variant=destructive]:focus:text-white
-      data-[variant=destructive]:*:[svg]:!text-destructive-foreground
-      data-[variant=destructive]:hover:*:[svg]:!text-white
+      data-[variant=destructive]:focus:text-gray-800
+      data-[variant=destructive]:*:[svg]:!text-gray-800
+      data-[variant=destructive]:hover:*:[svg]:!text-gray-800
       [&_svg:not([class*='text-'])]:text-muted-foreground 
-      relative flex cursor-default items-center gap-2 rounded-none px-2 py-1.5 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4`,
+      relative flex cursor-default items-center gap-2 rounded-none px-2 py-1.5 text-sm  hover:!outline-none outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 cursor-pointer`,
       props.class,
     )"
   >
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import './assets/styles/shadcn.css'
 import './assets/styles/main.css'
 import './assets/styles/tailwind.css'
+import './assets/styles/global.css'
 
 // 创建应用实例
 const app = createApp(App)
